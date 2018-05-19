e646db2c1c48dac2080b030341bfbaef571215ad
Chad Gray <chad.gray@traderinteractive.com>
2018-05-19 08:06:43 -0400

    Add phpdoc to Serializer::unserialize

--- src/Serializer.php
+++ src/Serializer.php
@@ -4,6 +4,14 @@ namespace SubjectivePHP\JSON;
 
 final class Serializer
 {
+    /**
+     * Converts a JSON-formatted string to an object of the specified type.
+     *
+     * @param string        $json   The JSON string to deserialize.
+     * @param string|object $object The class name or an object instance to which the json will be unserialized.
+     *
+     * @return UnserializableInterface
+     */
     public static function unserialize(string $json, $object) : UnserializableInterface
     {
         if (!is_subclass_of($object, UnserializableInterface::class)) {
